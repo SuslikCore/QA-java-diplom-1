f467b5d7a75d0151a05a99f140873af7c5648b61
Mark Grebenshchikov <marc.grebenshchikov@yandex.ru>
2025-01-12 05:23:29 -0500

    added final keyword in IngredientTest and reformat all the code again

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -11,11 +11,11 @@ import java.util.Collection;
 @RunWith(Parameterized.class)
 public class IngredientTest {
 
-    private IngredientType type;
-    private String name;
-    private float price;
+    private final IngredientType type;
+    private final String name;
+    private final float price;
 
-    public IngredientTest(IngredientType type, String name, float price){
+    public IngredientTest(IngredientType type, String name, float price) {
         this.type = type;
         this.name = name;
         this.price = price;
@@ -30,23 +30,22 @@ public class IngredientTest {
     }
 
 
-
     @Test
-    public void getTypeTest(){
-        Ingredient ingredient = new Ingredient(type,name,price);
+    public void getTypeTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
         Assert.assertEquals(type, ingredient.getType());
 
     }
 
     @Test
-    public void getNameTest(){
-        Ingredient ingredient = new Ingredient(type,name,price);
+    public void getNameTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
         Assert.assertEquals(name, ingredient.getName());
     }
 
     @Test
-    public void getPriceTest(){
-        Ingredient ingredient = new Ingredient(type,name,price);
-        Assert.assertEquals(price, ingredient.getPrice(),0.0f);
+    public void getPriceTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        Assert.assertEquals(price, ingredient.getPrice(), 0.0f);
     }
 }
